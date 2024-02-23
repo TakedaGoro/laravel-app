7539e61103eca8d0c022d8fb3a170e4f804f0c44
TakedaGoro <goro.takeda@divx.co.jp>
2024-02-23 12:19:14 +0900

    ログインしていないと投稿できないようにする

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -10,33 +10,35 @@
 </head>
 <body>
     <h1>つぶやきアプリ</h1>
-    <div>
-        <p>投稿フォーム</p>
-        <form action="{{ route('tweet.create') }}" method="post">
-            @csrf
-            <label for="tweet-content">つぶやき</label>
-            <span>140文字まで</span>
-            <textarea name="tweet" id="tweet-content" type="text" placeholder="つぶやき入力" ></textarea>
-            @error('tweet')
-                <p style="color: red;">{{ $message }}</p>
-            @enderror
-            <button type="submit">投稿</button>
-        </form>
-    </div>
-    <div>
-        @foreach ($tweets as $tweet)
-            <details>
-                <summary>{{ $tweet->content }}</summary>
-                <div>
-                    <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-                    <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
-                        @method('delete')
-                        @csrf
-                        <button type="submit">削除</button>
-                    </form>
-                </div>
-            </details>
-        @endforeach
-    </div>
+    @auth
+        <div>
+            <p>投稿フォーム</p>
+            <form action="{{ route('tweet.create') }}" method="post">
+                @csrf
+                <label for="tweet-content">つぶやき</label>
+                <span>140文字まで</span>
+                <textarea name="tweet" id="tweet-content" type="text" placeholder="つぶやき入力" ></textarea>
+                @error('tweet')
+                    <p style="color: red;">{{ $message }}</p>
+                @enderror
+                <button type="submit">投稿</button>
+            </form>
+        </div>
+    @endauth
+        <div>
+            @foreach ($tweets as $tweet)
+                <details>
+                    <summary>{{ $tweet->content }}</summary>
+                    <div>
+                        <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
+                        <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
+                            @method('delete')
+                            @csrf
+                            <button type="submit">削除</button>
+                        </form>
+                    </div>
+                </details>
+            @endforeach
+        </div>
 </body>
 </html>
